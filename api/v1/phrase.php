0b38f17558ff87931361ba2d17f32d44db44d942
Damian Ippolito <damian.ippolito@gmail.com>
2017-06-29 13:51:09 +0200

    creacion del .htaccess y del fichero que recibe las peticiones

---
+++ api/v1/phrase.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors',1);
+require_once '../../class/PhraseAPI.php';
+// Requests from the same server don't have a HTTP_ORIGIN header
+if (!array_key_exists('HTTP_ORIGIN', $_SERVER)) {
+    $_SERVER['HTTP_ORIGIN'] = $_SERVER['SERVER_NAME'];
+}
+
+try {
+    $API = new PhraseAPI($_REQUEST['request'], $_SERVER['HTTP_ORIGIN']);
+    echo $API->processAPI();
+} catch (Exception $e) {
+    echo json_encode(Array('error' => $e->getMessage()));
+}
+?>
